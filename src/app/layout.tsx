ca82b9da6a2dd8c1299826e9a575c3c1e59bcf96
Vladislav-jpg97 <sabiwaaa17@gmail.com>
2026-01-16 14:20:54 +0500

    fix: Было добавлено suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,33 +6,36 @@ import { Footer } from "@/layouts/footer"
 import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/utils"
 import { AosProvider } from "@/components/AosProvider";
+import { Suspense } from "react";
 
 
 
 export const montserrat = Montserrat({
-    subsets: ["latin", "cyrillic"],
-    weight: ["400", "600", "700"],
-    display: "swap"
+	subsets: ["latin", "cyrillic"],
+	weight: ["400", "600", "700"],
+	display: "swap"
 });
 
 
 const AppLayout: LayoutProps = ({ children }) => {
-    return <html lang="ru" className={cn(montserrat.className, "scroll-smooth")}>
-        <body className="flex flex-col min-h-screen justify-between bg-white text-black">
-            <AosProvider />
+	return <html lang="ru" className={cn(montserrat.className, "scroll-smooth")}>
+		<body className="flex flex-col min-h-screen justify-between bg-white text-black">
+			<Suspense fallback={<p>Loading...</p>}>
+				<AosProvider />
 
-            <Header />
+				<Header />
 
-            <main className="flex-1">
+				<main className="flex-1">
 
-                {children}
+					{children}
 
 
-            </main>
+				</main>
 
-            <Footer className="shrink-0" />
+				<Footer className="shrink-0" />
+			</Suspense>
 
-        </body>
-    </html>
+		</body>
+	</html>
 };
 export default AppLayout
